b302fe7ba12fc741b90a006cdd5bbf34595b134e
Michael Summers & Austin Kincaid <student@epicodus.com>
2015-08-03 16:00:00 -0700

    Add a spec and a method to declare wether or not a queen is in line with a specific position

--- lib/queen_attack.rb
+++ lib/queen_attack.rb
@@ -1,5 +1,16 @@
 class Array
-  define_method(:queen_attack) do
-    
+  define_method(:queen_attack?) do | coordinates |
+    queen_x_position = self[0]
+    queen_y_position = self[1]
+    input_x_position = coordinates[0]
+    input_y_position = coordinates[1]
+
+    if (queen_x_position === input_x_position)
+      return true
+    elsif (queen_y_position === input_y_position)
+      return true
+    else
+      return false
+    end
   end
 end
